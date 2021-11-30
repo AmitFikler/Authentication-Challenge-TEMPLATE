be8e2386c5e41671b92e74d9ee6270db5d17c202
AmitFikler <amitfikler@gmail.com>
2021-11-30 15:23:45 +0200

    build first router

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-/* write your server code here */
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+// const apiRoute = require('./routes/apiRoute');
+
+app.use(morgan('dev'));
+app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// app.use('/api', apiRoute);
+
+module.exports = app;
